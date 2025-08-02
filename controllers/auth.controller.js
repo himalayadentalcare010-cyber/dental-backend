1d59d612e173ed39932144494427fb9d90ada076
Sitaram <spoudel646@gmail.com>
2025-08-02 09:14:52 +0545

    fix login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -52,10 +52,15 @@ exports.login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(401).json({ error: 'Invalid credentials' });
 
-    const token = jwt.sign(
-      { userId: user.id },
-      '954f21bc7d7b0090450ab51a1490b5cb83c1cfffac45303d2776a716b71e8a0a'
-    );
+    if (!JWT_SECRET) {
+      console.error('JWT_SECRET is undefined!');
+      return res.status(500).json({ error: 'JWT secret is missing' });
+    }
+    let token;
+    try {
+      token = jwt.sign({ userId: user.id }, JWT_SECRET);
+    } catch (error) {}
+    res.status(500).json({ error: 'jwt error' });
 
     res.json({ token, user: { id: user.id, name: user.name, email: user.email } });
   } catch (error) {
