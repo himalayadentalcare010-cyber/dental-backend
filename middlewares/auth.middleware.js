046ba02b8e639bd32ad8b2f412c3e0c8e72ed267
Your Name <your.email@example.com>
2025-10-12 09:39:12 +0545

    test login api netlify

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -12,6 +12,6 @@ exports.requireAuth = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({ error: 'Invalid token' });
+    return res.status(401).json({ error: authHeader });
   }
 };
