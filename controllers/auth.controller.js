b6c071972f40c4105bcbed026b267e7216568053
Your Name <your.email@example.com>
2025-10-12 07:17:59 +0545

    test login api netlify

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -44,7 +44,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
-     const user = await prisma.user.findUniqueOrThrow({ where: { email } });
+     const user = await prisma.user.findMany();
     //  if (!user) return res.status(401).json({ error: 'Invalid credentials' });
 
     // const isMatch = await bcrypt.compare(password, user.password);
