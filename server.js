219a965085e1e0717b8e77f8677b35b90242753f
Sitaram poudel <53119905+sitaram-pdl@users.noreply.github.com>
2025-11-04 23:32:53 +0545

    Add additional origin to CORS configuration

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 app.use(
   cors({
-    origin: ['https://4gnepal.com.np', 'http://localhost:5173'], // allow both production and dev
+    origin: ['https://4gnepal.com.np', 'http://localhost:5173','https://4gnepal.com'], // allow both production and dev
     credentials: true, // if you send cookies or auth headers
   })
 );
