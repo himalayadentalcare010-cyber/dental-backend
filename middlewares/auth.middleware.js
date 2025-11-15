e704fd88cca17e7ae5960ea935c37cc94c62c349
Your Name <your.email@example.com>
2025-11-15 09:29:16 +0545

    fix : middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,13 +1,18 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
-exports.requireAuth = (event) => {
-  const authHeader = event.headers.authorization;
-
+exports.requireAuth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer "))
-    throw new Error("No token provided");
+    return res.status(401).json({ error: "No token provided" });
 
   const token = authHeader.split(" ")[1];
 
-  return jwt.verify(token, JWT_SECRET);
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: authHeader });
+  }
 };
